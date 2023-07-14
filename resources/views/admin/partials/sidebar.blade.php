6da02837a68529cb50e6c08d3e09fa7aa8110a82
vmhieu1211 <cavaldos1211@gmail.com>
2023-07-14 09:56:28 +0700

    aaaa

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,105 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Quản trị Website</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+ 
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link" href="change-password.html">
+            <i class="fas fa-fw fa-key"></i>
+            <span>Đổi mật khẩu</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="change-password.html">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Quản lý QTV</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="change-password.html">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Quản lý tài khoản</span>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Phân quyền</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="">Roles</a>
+                <a class="collapse-item" href="cards.html">Modules</a>
+                <a class="collapse-item" href="cards.html">Actions</a>
+                <a class="collapse-item" href="cards.html">Users</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item   -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePost"
+            aria-expanded="true" aria-controls="collapsePost">
+            <i class="fas fa-fw fa-bars"></i>
+            <span>Bài viết</span>
+        </a>
+        <div id="collapsePost" class="collapse" aria-labelledby="headingPost" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="Post-color.html">Colors</a>
+                <a class="collapse-item" href="Post-border.html">Borders</a>
+                <a class="collapse-item" href="Post-animation.html">Animations</a>
+                <a class="collapse-item" href="Post-other.html">Other</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSlider"
+            aria-expanded="true" aria-controls="collapseSlider">
+            <i class="fas fa-fw fa-bars"></i>
+            <span>Slider</span>
+        </a>
+        <div id="collapseSlider" class="collapse" aria-labelledby="headingPost" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="Slider-color.html">Colors</a>
+                <a class="collapse-item" href="Slider-border.html">Borders</a>
+                <a class="collapse-item" href="Slider-animation.html">Animations</a>
+                <a class="collapse-item" href="Slider-other.html">Other</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Cài đặt</span></a>
+    </li>
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-4">Welcome to my website</h1>
+        <p class="lead">This is the homepage of my website.</p>
+        <hr class="my-4">
+        <p>Learn more about what we do and how we can help you.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+@endsection
